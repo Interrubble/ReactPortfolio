a6e4ea25251a605de3e23c3b9d8514eb98d8c3ab
Interrubble <delagarzachris@icloud.com>
2022-05-31 15:02:48 -0700

    added components folder with pages folder, navtabs.js and portfoliocontainer.js inside components folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import './components/NavTabs';
+import Home from './components/pages/Home'
+import Dashboard from './components/pages/Dashboard';
+import Fitness from './components/pages/Fitness';
+import Sleep from './components/pages/Sleep';
+import Hydration from './components/pages/Hydration';
+import Mindfulness from './components/pages/Mindfulness';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/dashboard' element={<Dashboard/>}/>
+            <Route path='/fitness' element={<Fitness/>}/>
+            <Route path='/sleep' element={<Sleep/>}/>
+            <Route path='/hydration' element={<Hydration/>}/>
+            <Route path='/mindfulness' element={<Mindfulness/>}/>
+        </Routes>
+        </BrowserRouter>
+    // <h1>Hello</h1>
   );
 }
 
